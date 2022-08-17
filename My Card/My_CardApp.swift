b0778f47a98b7214bb29fe0d829e98c1e3f972ba
VMakhota <vmakhota@dr-telemed.ru>
2022-08-17 14:07:31 +0300

    Initial Commit

---
+++ My Card/My_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CardApp.swift
+//  My Card
+//
+//  Created by VMakhota on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
